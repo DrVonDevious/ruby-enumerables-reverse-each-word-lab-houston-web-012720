416293d0b225eabdb460c73dbe7c3143d658a6fb
William C Rutland <william.prometheus@gmail.com>
2020-01-05 17:34:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,6 @@ def reverse_each_word(string)
   sentence_array = string.split()
   sentence_array.each do |word|
     word = word.reverse()
-    sentence_array.push(word)
   end
   pp sentence_array
 end
